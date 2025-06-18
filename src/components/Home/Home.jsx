d426c6eb78bab3e3ad6e3c1a446c52d604861ca7
SujitaRijal <rijalsujeeta@gmail.com>
2025-06-18 14:39:11 +0545

    solve deploy error

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "../Hero/Hero";
 import About from "../About/About";
 import Explore from "../Explore/Explore";
 import Testimonial from "../Testimonials/Testimonial";
-import Contact from "../Contact/contact";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
   return (
